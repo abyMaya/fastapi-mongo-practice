56abef2355b7d4bf05ac4bbe40471660fd745934
abyMaya <abycat22@gmail.com>
2024-10-24 12:20:45 +0900

    [add]db_connection.py作成

---
+++ app/database/db_connection.py
@@ -0,0 +1,19 @@
+# app/database/db_connection.py
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+
+class Database:
+    def __init__(self):
+        self.client = None
+        self.database = None
+
+    async def connect(self):
+        load_dotenv()  # 環境変数をロード
+        self.client = AsyncIOMotorClient(os.getenv("MONGODB_URL"))
+        self.database = self.client.get_default_database()
+        return self.database
+
+    async def disconnect(self):
+        if self.client:
+            self.client.close()
