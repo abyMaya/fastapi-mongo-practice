2e623f48409ee07b68969859404acccfda2253d3
abyMaya <abycat22@gmail.com>
2024-10-24 18:11:48 +0900

    [update]main.py からAPI分離。動作確認済み

---
+++ app/api/user.py
@@ -0,0 +1,94 @@
+# app/api/user.py
+from fastapi import APIRouter, FastAPI, HTTPException
+from pydantic import BaseModel
+
+from app.models import Category, Character, Item, Series, User
+
+app = FastAPI
+router = APIRouter()
+
+class UserRequest(BaseModel):
+    user_name: str
+    email: str
+    password: str
+
+@router.post("/users/")
+async def create_user(user: UserRequest):
+    user = User(**user.model_dump())
+    await user.insert()
+    return user
+
+@router.get("/users/{user_id}")
+async def get_user(user_id: str):
+    user = await User.get(user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+# class ItemCreateRequest(BaseModel):
+#     item_name: str
+
+# @app.post("/items/")
+# async def create_item(item: ItemCreateRequest):
+#     item_document = Item(**item.model_dump())
+#     await item_document.insert()
+#     return item_document
+
+# @app.get("/items/{item_id}")
+# async def get_item(item_id: str):
+#     item = await Item.get(item_id)
+#     if not item:
+#         raise HTTPException(status_code=404, detail="Item not found")
+#     return item
+
+
+# class CategoryCreateRequest(BaseModel):
+#     category_name: str
+
+# @app.post("/categories/")
+# async def create_category(category: CategoryCreateRequest):
+#     category_document = Category(**category.model_dump())
+#     await category.insert()
+#     return category
+
+# @app.get("/categories/{category_id}")
+# async def get_category(category_id: str):
+#     category = await Category.get(category_id)
+#     if not category:
+#         raise HTTPException(status_code=404, detail="Category not found")
+#     return category
+
+
+# class SeriesCreateRequest(BaseModel):
+#     category_name: str
+
+# @app.post("/series/")
+# async def create_series(series: SeriesCreateRequest):
+#     Series = Series(**series.model_dump())
+#     await series.insert()
+#     return series
+
+# @app.get("/series/{series_id}")
+# async def get_series(series_id: str):
+#     series = await Series.get(series_id)
+#     if not series:
+#         raise HTTPException(status_code=404, detail="Series not found")
+#     return series
+
+
+# class CharactersCreateRequest(BaseModel):
+#     character_name: str
+
+# @app.post("/character/")
+# async def create_character(character: CharactersCreateRequest):
+#     character = Character(**character.model_dump())
+#     await character.insert()
+#     return character
+
+# @app.get("/character/{character_id}")
+# async def get_character(character_id: str):
+#     character = await Character.get(character_id)
+#     if not character:
+#         raise HTTPException(status_code=404, detail="Character not found")
+#     return character
+
--- app/main.py
+++ app/main.py
@@ -1,98 +1,17 @@
 # app/main.py
+from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException
 from app.init_schema import init_db, init_schema
 from app.models import Category, Item, Series, User, Character
 from pydantic import BaseModel
+from app.api.user import router as user_router  # ユーザー用のルーターをインポート
 
 app = FastAPI()
 
-class UserCreateRequest(BaseModel):
-    user_name: str
-    email: str
-    password: str
-
-@app.on_event("startup")
-async def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     await init_db()
     await init_schema()
 
-@app.post("/users/")
-async def create_user(user: UserCreateRequest):
-    user = User(**user.model_dump())
-    await user.insert()
-    return user
-
-@app.get("/users/{user_id}")
-async def get_user(user_id: str):
-    user = await User.get(user_id)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
-
-class ItemCreateRequest(BaseModel):
-    item_name: str
-
-@app.post("/items/")
-async def create_item(item: ItemCreateRequest):
-    item_document = Item(**item.model_dump())
-    await item_document.insert()
-    return item_document
-
-@app.get("/items/{item_id}")
-async def get_item(item_id: str):
-    item = await Item.get(item_id)
-    if not item:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return item
-
-
-class CategoryCreateRequest(BaseModel):
-    category_name: str
-
-@app.post("/categories/")
-async def create_category(category: CategoryCreateRequest):
-    category_document = Category(**category.model_dump())
-    await category.insert()
-    return category
-
-@app.get("/categories/{category_id}")
-async def get_category(category_id: str):
-    category = await Category.get(category_id)
-    if not category:
-        raise HTTPException(status_code=404, detail="Category not found")
-    return category
-
-
-class SeriesCreateRequest(BaseModel):
-    category_name: str
-
-@app.post("/series/")
-async def create_series(series: SeriesCreateRequest):
-    Series = Series(**series.model_dump())
-    await series.insert()
-    return series
-
-@app.get("/series/{series_id}")
-async def get_series(series_id: str):
-    series = await Series.get(series_id)
-    if not series:
-        raise HTTPException(status_code=404, detail="Series not found")
-    return series
-
-
-class CharactersCreateRequest(BaseModel):
-    character_name: str
-
-@app.post("/character/")
-async def create_character(character: CharactersCreateRequest):
-    character = Character(**character.model_dump())
-    await character.insert()
-    return character
-
-@app.get("/character/{character_id}")
-async def get_character(character_id: str):
-    character = await Character.get(character_id)
-    if not character:
-        raise HTTPException(status_code=404, detail="Character not found")
-    return character
-
+# ルーターを追加
+app.include_router(user_router)  # プレフィックスなしのユーザーエンドポイント
