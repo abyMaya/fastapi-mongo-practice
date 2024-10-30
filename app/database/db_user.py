f94ce8a33c449050976b9e7fc4920ea38e7c482d
abyMaya <abycat22@gmail.com>
2024-10-31 05:57:14 +0900

    [update]user.pyから分割

--- app/database/db_user.py
+++ app/database/db_user.py
@@ -9,3 +9,8 @@ async def create_user(user: User):
     await db.connect()
     await user.insert()
     return user
+
+async def get_user(user_id: str):
+    await db.connect()
+    user = await User.get(user_id)
+    return user
